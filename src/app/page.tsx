f97f31ff2327bf2bd3ab421fb8e6c61ad369a0e0
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-08-27 22:10:56 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,24 @@ export default function Home() {
     return <div>Loading...</div>;
   }
   return (
-    <main className="bg-stone-600 bg-cover h-[1080px] w-full">
-      <h1 className="text-neutral-300 font-bold text-4xl flex justify-center">
-        RtK Query
-      </h1>
+    <main>
+      <div className="bg-gray-900 min-h-screen p-6">
+        <div className="max-w-3xl mx-auto bg-gray-900  rounded-lg shadow-md p-6">
+          <h1 className="text-neutral-300 font-bold text-4xl flex justify-center">
+            RtK Query
+          </h1>
 
-      <div className="grid grid-cols-2 gap-4 mt-6 border-2 border-gray-700 m-2 p-2">
-        {data.map((item: UserData) => (
-          <div key={item.id} className="p-4 bg-gray-800 rounded shadow-md">
-            <h2 className="text-lg font-semibold">{item.name}</h2>
-            <p className="text-gray-500">{item.email}</p>
-            <p className="mt-2">{item.company.catchPhrase}</p>
-            <p className="mt-2">{item.address.city}</p>
+          <div className="grid grid-cols-2 gap-4 mt-6 border-2 border-gray-700 m-2 p-2">
+            {data.map((item: UserData) => (
+              <div key={item.id} className="p-4 bg-gray-800 rounded shadow-md">
+                <h2 className="text-lg font-semibold">{item.name}</h2>
+                <p className="text-gray-500">{item.email}</p>
+                <p className="mt-2">{item.company.catchPhrase}</p>
+                <p className="mt-2">{item.address.city}</p>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </main>
   );
